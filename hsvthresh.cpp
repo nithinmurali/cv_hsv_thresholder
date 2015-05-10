24c83d1d03537e72cfeedb7063af20722dbfc45a
nithin murali <imnmfotmal@gmail.com>
2015-05-10 16:44:22 +0530

    added saving option

--- hsvthresh.cpp
+++ hsvthresh.cpp
@@ -2,6 +2,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <stdlib.h>
+#include <fstream>
 
 using namespace cv;
 using namespace std;
@@ -24,33 +25,48 @@ using namespace std;
   
   int run = atoi(argv[2]);
 
-    namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+  namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+  int iLowH,iHighH;
+  int iLowS,iHighS;
+  int iLowV,iHighV;
 
-  int iLowH = 0;
- int iHighH = 179;
+  fstream cfile;
+  cfile.open(".hsvConfig", std::fstream::in | std::fstream::out | std::fstream::app);
+  if( cfile.peek() == std::ifstream::traits_type::eof() )
+  {
+   	cout<<"empty file"<<endl;
+    iLowH = 0;
+    iHighH = 179;
 
-  int iLowS = 0; 
- int iHighS = 255;
+    iLowS = 0; 
+    iHighS = 255;
 
-  int iLowV = 0;
- int iHighV = 255;
-
+    iLowV = 0;
+    iHighV = 255;
+  }
+  else
+  {
+     cfile>>iLowH>>iHighH>>iLowS>>iHighS>>iLowV>>iHighV;
+     cout<<"config read"<<endl;
+  }
+  cfile.close();
+  
   //Create trackbars in "Control" window
- cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
- cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+  cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+  cvCreateTrackbar("HighH", "Control", &iHighH, 179);
 
   cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
- cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+  cvCreateTrackbar("HighS", "Control", &iHighS, 255);
 
   cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
- cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+  cvCreateTrackbar("HighV", "Control", &iHighV, 255);
 
     int i =1;
     while (true)
     {
       
       int k = waitKey(30);
-      cout<<k<<endl;
+      //cout<<k<<endl;
       if (type == 2 && i == 1)
       {
         
@@ -89,10 +105,15 @@ using namespace std;
   }*/
         if ( k == 27|| k == 1048603 ) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
        {
-            cout << "esc key is pressed by user" << endl;
-            break; 
-       }
-        else if ( k == 110 || k == 1048686 )
+        cout << "esc key is pressed by user" << endl;
+    
+        cfile.open(".hsvConfig", std::fstream::out | std::fstream::trunc);
+   	    cfile<<iLowH<<" "<<iHighH<<" "<<iLowS<<" "<<iHighS<<" "<<iLowV<<" "<<iHighV;
+        cout<<"saved config . . ."<<endl;
+  	    cfile.close();
+        break; 
+      }
+      else if ( k == 110 || k == 1048686 )
        {
             i=1;
        }
